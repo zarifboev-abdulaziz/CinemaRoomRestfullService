03a187e2165ac99cabaf1ffc0f5e1dbe0c38fa8b
Abdulaziz Zarifboyev <zarifboev.abdulaziz@gmail.com>
2022-03-14 15:16:26 +0500

    Initial commit

---
+++ src/main/java/uz/pdp/cinemaroomrestfullservice/CinemaRoomRestfullServiceApplication.java
@@ -0,0 +1,13 @@
+package uz.pdp.cinemaroomrestfullservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaRoomRestfullServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CinemaRoomRestfullServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/pdp/cinemaroomrestfullservice/CinemaRoomRestfullServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.pdp.cinemaroomrestfullservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaRoomRestfullServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

f294ddbff462a46c209757afa84c51af77f9930d
Abdulaziz Zarifboyev <zarifboev.abdulaziz@gmail.com>
2022-03-16 21:35:54 +0500

    Reserved Hall CRUD Functions are done

---
+++ src/main/java/uz/pdp/cinemaroomrestfullservice/repository/sessionRelatedRepositories/SessionDateRepository.java
@@ -0,0 +1,15 @@
+package uz.pdp.cinemaroomrestfullservice.repository.sessionRelatedRepositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import uz.pdp.cinemaroomrestfullservice.entity.movieSessionPack.SessionDate;
+
+import java.util.Date;
+import java.util.Optional;
+
+@Repository
+public interface SessionDateRepository extends JpaRepository<SessionDate, Long> {
+
+    Optional<SessionDate> findByDate(Date date);
+
+}
---
+++ src/main/java/uz/pdp/cinemaroomrestfullservice/service/sessionRelatedServices/ReservedHallService.java
@@ -0,0 +1,86 @@
+package uz.pdp.cinemaroomrestfullservice.service.sessionRelatedServices;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import uz.pdp.cinemaroomrestfullservice.entity.cinemaPack.Hall;
+import uz.pdp.cinemaroomrestfullservice.entity.movieSessionPack.Afisha;
+import uz.pdp.cinemaroomrestfullservice.entity.movieSessionPack.ReservedHall;
+import uz.pdp.cinemaroomrestfullservice.entity.movieSessionPack.SessionDate;
+import uz.pdp.cinemaroomrestfullservice.entity.movieSessionPack.SessionTime;
+import uz.pdp.cinemaroomrestfullservice.payload.ApiResponse;
+import uz.pdp.cinemaroomrestfullservice.payload.sessionRelatedPayloads.ReservedHallDto;
+import uz.pdp.cinemaroomrestfullservice.repository.cinemaRelatedRepositories.HallRepository;
+import uz.pdp.cinemaroomrestfullservice.repository.sessionRelatedRepositories.AfishaRepository;
+import uz.pdp.cinemaroomrestfullservice.repository.sessionRelatedRepositories.ReservedHallRepository;
+import uz.pdp.cinemaroomrestfullservice.repository.sessionRelatedRepositories.SessionDateRepository;
+import uz.pdp.cinemaroomrestfullservice.repository.sessionRelatedRepositories.SessionTimeRepository;
+
+import javax.xml.crypto.Data;
+import java.sql.Date;
+import java.sql.Time;
+import java.util.Optional;
+
+@Service
+public class ReservedHallService {
+    @Autowired
+    AfishaRepository afishaRepository;
+    @Autowired
+    HallRepository hallRepository;
+    @Autowired
+    SessionDateRepository sessionDateRepository;
+    @Autowired
+    SessionTimeRepository sessionTimeRepository;
+    @Autowired
+    ReservedHallRepository reservedHallRepository;
+
+
+
+
+    public ApiResponse saveReservedHall(ReservedHallDto reservedHallDto) {
+        Optional<Afisha> optionalAfisha = afishaRepository.findById(reservedHallDto.getAfishaId());
+        if (!optionalAfisha.isPresent())
+            return new ApiResponse("Afisha not found", false);
+
+        Optional<Hall> optionalHall = hallRepository.findById(reservedHallDto.getHallId());
+        if (!optionalHall.isPresent())
+            return new ApiResponse("Hall not found", false);
+
+        Optional<SessionDate> optionalSessionDate = sessionDateRepository.findByDate(reservedHallDto.getStartDate());
+        SessionDate startDate = new SessionDate();
+        if (!optionalSessionDate.isPresent()){
+            Date date = reservedHallDto.getStartDate();
+            startDate.setDate(date);
+        } else {
+            startDate = optionalSessionDate.get();
+        }
+
+        Optional<SessionTime> sessionStartTimeOptional = sessionTimeRepository.findByTime(reservedHallDto.getStartTime());
+        SessionTime startTime = new SessionTime();
+        if (!sessionStartTimeOptional.isPresent()){
+            Time time = reservedHallDto.getStartTime();
+            startTime.setTime(time);
+        } else {
+            startTime = sessionStartTimeOptional.get();
+        }
+
+        Optional<SessionTime> sessionEndTimeOptional = sessionTimeRepository.findByTime(reservedHallDto.getEndTime());
+        SessionTime endTime = new SessionTime();
+        if (!sessionEndTimeOptional.isPresent()){
+            Time time = reservedHallDto.getEndTime();
+            endTime.setTime(time);
+        }else {
+            endTime = sessionEndTimeOptional.get();
+        }
+
+        ReservedHall reservedHall = new ReservedHall();
+        reservedHall.setId(reservedHallDto.getId());
+        reservedHall.setHall(optionalHall.get());
+        reservedHall.setAfisha(optionalAfisha.get());
+        reservedHall.setStartDate(startDate);
+        reservedHall.setStartTime(startTime);
+        reservedHall.setEndTime(endTime);
+        ReservedHall savedReservedHall = reservedHallRepository.save(reservedHall);
+
+        return new ApiResponse("Reserve Hall Successfully Saved", true, savedReservedHall);
+    }
+}

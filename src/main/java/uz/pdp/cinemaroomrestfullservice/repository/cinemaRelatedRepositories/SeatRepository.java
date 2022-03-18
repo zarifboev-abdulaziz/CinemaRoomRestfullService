e30146700cefd18226dc2bc51532299e5e615fd1
Abdulaziz Zarifboyev <zarifboev.abdulaziz@gmail.com>
2022-03-18 09:07:54 +0500

    seat crud functions are done

--- src/main/java/uz/pdp/cinemaroomrestfullservice/repository/cinemaRelatedRepositories/SeatRepository.java
+++ src/main/java/uz/pdp/cinemaroomrestfullservice/repository/cinemaRelatedRepositories/SeatRepository.java
@@ -4,8 +4,22 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import uz.pdp.cinemaroomrestfullservice.entity.cinemaPack.Seat;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface SeatRepository extends JpaRepository<Seat, Long> {
 
     void deleteByRowId(Long row_id);
+
+    List<Seat> findByRowId(Long row_id);
+
+    Optional<Seat> findByIdAndRowId(Long id, Long row_id);
+
+    void deleteByIdAndRowId(Long id, Long row_id);
+
+    boolean existsByNumberAndRowId(Integer number, Long row_id);
+
+    boolean existsByNumberAndRowIdAndIdNot(Integer number, Long row_id, Long id);
+
 }
